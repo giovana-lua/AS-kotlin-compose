d44772d60200a0836d9e79f7f48a98529503bee2
GIOVANA BARBOSA SOUZA <25133664@dsn127m1242314.cfp127aluno.local>
2026-02-23 15:02:30 -0300

    colocando caixa de texto

--- app/src/main/java/com/aulasandroid/kotlincompose/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/kotlincompose/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.aulasandroid.kotlincompose
 
+import android.R.attr.contentDescription
+import android.R.attr.text
 import android.R.attr.value
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aulasandroid.kotlincompose.ui.theme.KotlinComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +43,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding( 32.dp)
+
+
+    ) {
         OutlinedTextField(
             value = "",
-            onValueChange = {}
+            onValueChange = {},
+            placeholder = {
+
+            Text("Oque você está procurando?")
+
+            },
+            trailingIcon =   {
+                Icon(
+                painter = painterResource(R.drawable.baseline_adjust_24),
+                contentDescription = ""
+                )
+            }
         )
     }
 }
